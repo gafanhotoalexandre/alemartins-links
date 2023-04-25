c5d46edd07f8c16d29d571c2df302b2fc9ce67ae
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-04-25 20:48:30 -0300

    remove console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,11 @@ export const metadata = {
   title: 'Ale Martins',
   description: 'Cartão de Visitas',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/icon.png',
   },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  console.log(inter.className)
   return (
     <html lang="pt-br" className="dark">
       <body className={inter.className}>{children}</body>

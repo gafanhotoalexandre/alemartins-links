86c22267433a2672b127db2449d22e2ac553839f
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-04-26 10:46:05 -0300

    add switcher hover

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -13,7 +13,7 @@ export function Switcher() {
         onClick={handleToggleTheme}
         className="w-16 h-6 relative rounded-full border dark:border-stroke border-stroke-light dark:bg-surface bg-surface-light"
       >
-        <Switch.Thumb className="block w-8 h-8 bg-white dark:bg-moon-stars bg-sun bg-no-repeat bg-center rounded-full -translate-y-1/2 absolute data-[state=checked]:translate-x-full data-[state=unchecked]:translate-x-0 transition-transform" />
+        <Switch.Thumb className="block w-8 h-8 bg-white dark:bg-moon-stars bg-sun bg-no-repeat bg-center rounded-full -translate-y-1/2 absolute data-[state=checked]:translate-x-full data-[state=unchecked]:translate-x-[-2px] transition-all hover:outline hover:outline-8 dark:hover:outline-stroke hover:outline-stroke-light" />
       </Switch.Root>
     </div>
   )
